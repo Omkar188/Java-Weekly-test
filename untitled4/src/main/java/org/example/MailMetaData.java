1f5e6299f1bc0a3bfab267aaa1a50640ec5df20b
Omkar <omkarkore1431@gmail.com>
2023-06-11 21:43:48 +0530

    Password issue solved

--- untitled4/src/main/java/org/example/MailMetaData.java
+++ untitled4/src/main/java/org/example/MailMetaData.java
@@ -7,7 +7,7 @@ public class MailMetaData {
     public static final String sslProperty="mail.smtp.ssl.enable";
     public static final String authPerm="mail.smtp.auth";
 
-    public static final String myUserMail="gpkp1522256@gmail.com";
+    public static final String myUserMail="gpkp152226@gmail.com";
 
     public static final String myPass="ckbmvepvtjejjoyu";
     public static final String receiverMail="omkarkore1431@gmail.com";
