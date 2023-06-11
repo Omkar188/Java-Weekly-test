220273bac134b3ff01e6756193fb761c921623e1
Omkar <omkarkore1431@gmail.com>
2023-06-11 21:27:07 +0530

    weekly test

---
+++ untitled4/src/main/java/org/example/MailMetaData.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class MailMetaData {
+    public static final String HostServer ="smtp.gmail.com";
+    public static final String port="465";
+
+    public static final String sslProperty="mail.smtp.ssl.enable";
+    public static final String authPerm="mail.smtp.auth";
+
+    public static final String myUserMail="gpkp1522256@gmail.com";
+
+    public static final String myPass="";
+    public static final String receiverMail="omkarkore1431@gmail.com";
+
+}
